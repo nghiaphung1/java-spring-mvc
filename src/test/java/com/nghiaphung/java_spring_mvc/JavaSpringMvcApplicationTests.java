7abd46d6d1d0f4554463d12bddc6a9b47cda917e
Phung Minh Nghia <nghiapmhe182314@fpt.edu.vn>
2025-04-03 16:59:59 +0700

    Git initial

---
+++ src/test/java/com/nghiaphung/java_spring_mvc/JavaSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nghiaphung.java_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
